f984ba7ccae842370e19a365548c0cec809e4343
acethepace <akshat14009@iiitd.ac.in>
2016-08-18 00:03:04 +0530

    handled rotation

--- app/src/main/java/com/mallock/primemaster/MainActivity.java
+++ app/src/main/java/com/mallock/primemaster/MainActivity.java
@@ -10,16 +10,34 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private final static int MIN_VALUE = 1;
     private final static int MAX_VALUE = 1000;
+    private static final String NUMBER_GENERATOR = "NUMBER_GENERATOR";
 
     private TextView primeNumberTV;
-    private final NumberGenerator numberGenerator = new NumberGenerator(MIN_VALUE, MAX_VALUE);
+    private NumberGenerator numberGenerator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        primeNumberTV = (TextView) findViewById(R.id.tv_prime);
-        loadNewNumber(null);
+        primeNumberTV = (TextView) findViewById(R.id.textView);
+        if (savedInstanceState == null) {
+            numberGenerator = new NumberGenerator(MIN_VALUE, MAX_VALUE);
+            loadNewNumber(null);
+        } else {
+            numberGenerator = (NumberGenerator) savedInstanceState.get(NUMBER_GENERATOR);
+            primeNumberTV.setText(numberGenerator.getCurrentNumber()+"");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelable(NUMBER_GENERATOR, numberGenerator);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
     }
 
     @Override
@@ -28,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loadNewNumber(@Nullable View view) {
-        primeNumberTV.setText(numberGenerator.generateNew()+"");
+        primeNumberTV.setText(numberGenerator.generateNew() + "");
     }
 
     public void answerNo(View view) {
--- app/src/main/java/com/mallock/primemaster/NumberGenerator.java
+++ app/src/main/java/com/mallock/primemaster/NumberGenerator.java
@@ -1,16 +1,36 @@
 package com.mallock.primemaster;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Mallock on 14-08-2016.
  */
-class NumberGenerator {
-    private int currentNumber;
+class NumberGenerator implements Parcelable {
+    public static final Creator<NumberGenerator> CREATOR = new Creator<NumberGenerator>() {
+        @Override
+        public NumberGenerator createFromParcel(Parcel in) {
+            return new NumberGenerator(in);
+        }
+
+        @Override
+        public NumberGenerator[] newArray(int size) {
+            return new NumberGenerator[size];
+        }
+    };
     private final double maxNumber;
     private final double minNumber;
+    private int currentNumber;
 
     public NumberGenerator(int minNumber, int maxNumber) {
         this.minNumber = minNumber;
         this.maxNumber = maxNumber;
+    }
+
+    protected NumberGenerator(Parcel in) {
+        currentNumber = in.readInt();
+        maxNumber = in.readDouble();
+        minNumber = in.readDouble();
     }
 
     public int getCurrentNumber() {
@@ -23,5 +43,17 @@ class NumberGenerator {
             newNumber = (int) (Math.random() * (maxNumber - minNumber));
         this.currentNumber = newNumber;
         return newNumber;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(currentNumber);
+        parcel.writeDouble(maxNumber);
+        parcel.writeDouble(minNumber);
     }
 }
